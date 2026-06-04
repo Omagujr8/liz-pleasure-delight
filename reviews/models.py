9c53c873888abd107a843a32d64e1054d374dd61
Omagu Emmanuel <omagujr8@gmail.com>
2026-06-04 23:25:41 +0100

    configure urls.py and created products/reviews/orders models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Review(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    customer_name = models.CharField(
+        max_length=100
+    )
+
+    rating = models.IntegerField()
+
+    comment = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.customer_name
