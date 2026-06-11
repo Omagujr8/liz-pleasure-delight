8cdbbf1a0116d3d41339c890e1c7ec02185986bf
Omagu Emmanuel <omagujr8@gmail.com>
2026-06-11 23:46:50 +0100

    Added API routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import path
+from products.views import ProductListAPIView, ProductDetailAPIView
+from reviews.views import ReviewListCreateAPIView
+from orders.views import OrderListCreateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # PRODUCTS
+    path('api/products/', ProductListAPIView.as_view()),
+    path('api/products/<int:pk>/', ProductDetailAPIView.as_view()),
+
+    # REVIEWS
+    path('api/reviews/', ReviewListCreateAPIView.as_view()),
+
+    # ORDERS
+    path('api/orders/', OrderListCreateAPIView.as_view()),
 ]
