060fdc3f839017b30fef2c3e232bad7907b30cd4
Omagu Emmanuel <omagujr8@gmail.com>
2026-06-11 23:42:14 +0100

    Created API views

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+
+class OrderListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
