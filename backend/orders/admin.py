9d0aa123032550cdd8230274fdcdee6b44806acc
Omagu Emmanuel <omagujr8@gmail.com>
2026-06-21 20:46:07 +0100

    Created order model, cart model, coupon system, payment model

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Order
+from .models import (
+    Order,
+    Cart,
+    CartItem,
+    OrderItem,
+    Coupon,
+    Payment
+)
 
 admin.site.register(Order)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(OrderItem)
+admin.site.register(Coupon)
+admin.site.register(Payment)
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-from products.models import Product
 from django.contrib.auth.models import User
-from django.db import models
 from products.models import Product
-
 
 class Order(models.Model):
 
     STATUS_CHOICES = (
         ('pending', 'Pending'),
+        ('paid', 'Paid'),
         ('processing', 'Processing'),
         ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
     )
 
     customer_name = models.CharField(
@@ -75,3 +79,79 @@ class CartItem(models.Model):
 
     def subtotal(self):
         return self.product.price * self.quantity
+    
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
+    
+class Coupon(models.Model):
+
+    code = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    discount_percent = models.IntegerField()
+
+    active = models.BooleanField(
+        default=True
+    )
+
+    def __str__(self):
+        return self.code
+    
+class Payment(models.Model):
+
+    PAYMENT_METHODS = (
+        ('flutterwave', 'Flutterwave'),
+        ('paystack', 'Paystack'),
+    )
+
+    order = models.OneToOneField(
+        Order,
+        on_delete=models.CASCADE
+    )
+
+    reference = models.CharField(
+        max_length=200
+    )
+
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHODS
+    )
+
+    paid = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.reference
