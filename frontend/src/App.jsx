6ee4cf92c38609fc6df28f23fff387e3c8a20a1c
Omagu Emmanuel <omagujr8@gmail.com>
2026-09-03 10:04:33 +0100

    Added images of products and logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,26 +10,26 @@ import Reviews from "./pages/Reviews";
 import Cart from "./pages/Cart";
 import Order from "./pages/Order";
 
-function App() {
-  return (
-    <>
-      <Navbar />
+      function App() {
+        return (
+          <>
+            <Navbar />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/snacks-mix" element={<SnacksMix />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/reviews" element={<Reviews />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<Order />} />
-        </Routes>
-      </main>
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/snacks-mix" element={<SnacksMix />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/reviews" element={<Reviews />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/order" element={<Order />} />
+              </Routes>
+            </main>
 
-      <Footer />
-    </>
-  );
-}
+            <Footer />
+          </>
+        );
+      }
 
-export default App;
+      export default App;
