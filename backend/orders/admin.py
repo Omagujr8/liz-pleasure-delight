2f6c925112a5dbaa35cccfb473ffe153a07ef547
Omagu Emmanuel <omagujr8@gmail.com>
2026-09-09 02:30:43 +0100

    Added admin templates for viewing orders

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -8,7 +8,15 @@ from .models import (
     Payment
 )
 
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/orders/order/change_list.html'
+    list_filter = ('status', 'scheduled_date', 'created_at')
+    search_fields = ('customer_name', 'phone_number', 'address')
+    list_display = ('id', 'customer_name', 'phone_number', 'total_price', 'status', 'scheduled_date', 'created_at')
+    ordering = ('-created_at',)
+
+
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(OrderItem)
